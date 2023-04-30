cf031acd2ebe65322ea8ec6e3e66e057f818f867
Andrey Oleynikov <oleynikovandreyvladimirovich@gmail.com>
2023-04-30 11:51:34 +0300

    Initial commit

---
+++ Assets/UIGradientButton/Shaders/Editor/ConditionalMaterialPropertyDrawer.cs
@@ -0,0 +1,188 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace UnityEditor.Rendering.Universal.ShaderGUI
+{
+    public abstract class ConditionalMaterialPropertyDrawer : MaterialPropertyDrawer
+    {
+        protected abstract bool ShowIfKeywordSet { get; }
+        protected abstract bool IsFloat { get; }
+
+        protected virtual bool IsFloatValid(float val1, float val2)
+        {
+            return false;
+        }
+
+        private string _keyword;
+
+        public ConditionalMaterialPropertyDrawer(string keyword)
+        {
+            _keyword = keyword;
+        }
+
+        public override void OnGUI(Rect position, MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            if (IsVisible(prop, label, editor))
+            {
+                editor.DefaultShaderProperty(position, prop, label);
+            }
+        }
+
+        public override float GetPropertyHeight(MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            if (IsVisible(prop, label, editor))
+            {
+                return base.GetPropertyHeight(prop, label, editor);
+            }
+            
+            return -2f; // invert default line height
+        }
+
+        private bool IsVisible(MaterialProperty prop, string label, MaterialEditor editor)
+        {
+            var isKeywordEnabled = false;
+            var isValidFloat = false;
+            var floatProperty = "";
+            var floatValue = 0f;
+
+            if (IsFloat)
+            {
+                var keywordChunks = _keyword.Split('#');
+
+                if (keywordChunks.Length > 1 && float.TryParse(keywordChunks[1], out floatValue))
+                {
+                    floatProperty = keywordChunks[0];
+                    isValidFloat = true;
+                }
+            }
+            
+            for (var i = 0; i < editor.targets.Length; i++)
+            {
+                var mat = editor.targets[i] as Material;
+                if (mat != null)
+                {
+                    if (!IsFloat)
+                    {
+                        isKeywordEnabled |= mat.IsKeywordEnabled(_keyword);
+                    }
+                    else if (isValidFloat)
+                    {
+                        isKeywordEnabled |= IsFloatValid(floatValue, mat.GetFloat(floatProperty));
+                    }
+                }
+            }
+
+            return ShowIfKeywordSet == isKeywordEnabled;
+        }
+    }
+
+    public class ShowIfEnabled : ConditionalMaterialPropertyDrawer
+    {
+        public ShowIfEnabled(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => true;
+        protected override bool IsFloat => false;
+    }
+    
+    public class ShowIfDisabled : ConditionalMaterialPropertyDrawer
+    {
+        public ShowIfDisabled(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => false;
+        protected override bool IsFloat => false;
+    }
+    
+    public class HideIfEnabled : ConditionalMaterialPropertyDrawer
+    {
+        public HideIfEnabled(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => false;
+        protected override bool IsFloat => false;
+    }
+    
+    public class HideIfDisabled : ConditionalMaterialPropertyDrawer
+    {
+        public HideIfDisabled(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => true;
+        protected override bool IsFloat => false;
+    }
+    
+    public class ShowIfGreater : ConditionalMaterialPropertyDrawer
+    {
+        public ShowIfGreater(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => true;
+        protected override bool IsFloat => true;
+        protected override bool IsFloatValid(float val1, float val2)
+        {
+            return val1 < val2;
+        }
+    }
+    
+    public class ShowIfGreaterEqual : ConditionalMaterialPropertyDrawer
+    {
+        public ShowIfGreaterEqual(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => true;
+        protected override bool IsFloat => true;
+        protected override bool IsFloatValid(float val1, float val2)
+        {
+            return val1 <= val2;
+        }
+    }
+    
+    public class ShowIfEqual : ConditionalMaterialPropertyDrawer
+    {
+        public ShowIfEqual(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => true;
+        protected override bool IsFloat => true;
+        protected override bool IsFloatValid(float val1, float val2)
+        {
+            return Mathf.Approximately(val1, val2);
+        }
+    }
+    
+    public class ShowIfLess : ConditionalMaterialPropertyDrawer
+    {
+        public ShowIfLess(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => true;
+        protected override bool IsFloat => true;
+        protected override bool IsFloatValid(float val1, float val2)
+        {
+            return val1 > val2;
+        }
+    }
+    
+    public class ShowIfLessEqual : ConditionalMaterialPropertyDrawer
+    {
+        public ShowIfLessEqual(string keyword) : base(keyword)
+        {
+        }
+
+        protected override bool ShowIfKeywordSet => true;
+        protected override bool IsFloat => true;
+        protected override bool IsFloatValid(float val1, float val2)
+        {
+            return val1 >= val2;
+        }
+    }
+}
---
+++ Assets/UIGradientButton/Shaders/Editor/ParticlesUnlitShader.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityEditor.Rendering.Universal.ShaderGUI
+{
+    internal class CWParticlesUnlitShader : BaseShaderGUI
+    {
+        // Properties
+        private BakedLitGUI.BakedLitProperties shadingModelProperties;
+        private ParticleGUI.ParticleProperties particleProps;
+
+        // List of renderers using this material in the scene, used for validating vertex streams
+        List<ParticleSystemRenderer> m_RenderersUsingThisMaterial = new List<ParticleSystemRenderer>();
+
+        public override void FindProperties(MaterialProperty[] properties)
+        {
+            base.FindProperties(properties);
+            shadingModelProperties = new BakedLitGUI.BakedLitProperties(properties);
+            particleProps = new ParticleGUI.ParticleProperties(properties);
+        }
+
+        public override void ValidateMaterial(Material material)
+        {
+            SetMaterialKeywords(material, null, ParticleGUI.SetMaterialKeywords);
+        }
+
+        public override void DrawSurfaceOptions(Material material)
+        {
+            base.DrawSurfaceOptions(material);
+            DoPopup(ParticleGUI.Styles.colorMode, particleProps.colorMode, Enum.GetNames(typeof(ParticleGUI.ColorMode)));
+        }
+
+        public override void DrawSurfaceInputs(Material material)
+        {
+            base.DrawSurfaceInputs(material);
+            BakedLitGUI.Inputs(shadingModelProperties, materialEditor);
+            DrawEmissionProperties(material, true);
+        }
+
+        public override void DrawAdvancedOptions(Material material)
+        {
+            materialEditor.ShaderProperty(particleProps.flipbookMode, ParticleGUI.Styles.flipbookMode);
+            ParticleGUI.FadingOptions(material, materialEditor, particleProps);
+            ParticleGUI.DoVertexStreamsArea(material, m_RenderersUsingThisMaterial);
+
+            DrawQueueOffsetField();
+        }
+        
+        public override void DrawBaseProperties(Material material)
+        {
+            if (baseMapProp != null && baseColorProp != null) // Draw the baseMap, most shader will have at least a baseMap
+            {
+                //materialEditor.TexturePropertySingleLine( Styles.baseMap, baseMapProp, baseColorProp);
+                materialEditor.TextureProperty(baseMapProp, Styles.baseMap.text );
+                materialEditor.ColorProperty(baseColorProp, Styles.baseMap.text + " Color" );
+            }
+        }
+
+        public override void OnOpenGUI(Material material, MaterialEditor materialEditor)
+        {
+            CacheRenderersUsingThisMaterial(material);
+            base.OnOpenGUI(material, materialEditor);
+        }
+
+        void CacheRenderersUsingThisMaterial(Material material)
+        {
+            m_RenderersUsingThisMaterial.Clear();
+
+            ParticleSystemRenderer[] renderers = UnityEngine.Object.FindObjectsOfType(typeof(ParticleSystemRenderer)) as ParticleSystemRenderer[];
+            foreach (ParticleSystemRenderer renderer in renderers)
+            {
+                if (renderer.sharedMaterial == material)
+                    m_RenderersUsingThisMaterial.Add(renderer);
+            }
+        }
+    }
+} 
